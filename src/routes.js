de63aa93c39a84fe95dfc6f944c248a3e94bbd73
Achmad Setiaman <achmadsetiaman@gmail.com>
2023-05-21 16:58:54 +0700

    add feature show

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,20 @@
-const { addNoteHandler } = require('./handler');
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require('./handler');
 
 const routes = [
   {
     method: 'POST',
     path: '/notes',
     handler: addNoteHandler,
-    options: {
-      cors: {
-        origin: ['*'],
-      },
-    },
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
   },
 ];
 
